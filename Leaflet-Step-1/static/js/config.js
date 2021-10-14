b5b1a011ea2b569e23593426b9058e2004340d0c
Ali Krakowsky <alig_10@comcast.net>
2021-10-14 14:58:50 -0700

    update logic and config

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiYWtyYWtvd3NreSIsImEiOiJja3VyaDcxdmc1NTZrMnBxNjhiNmxrMXZjIn0.tcCsN73j3apksjuvgqiLKQ";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,35 @@
 // Create a URL for the site
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-// Production url
-"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+// Add the earthquake data
+d3.json(url).then(function(data) {
+    createFeatures(data.features);
+});
 
+// Create a marker
+function markerSize(circle) {
+    return circle * 5;
+}
+
+// Create a loop to determine marker size
+function chooseColor(depth) {
+    if (depth < 10) {
+        return "green"
+    }
+    else if (depth < 30) {
+        return "limegreen"
+    }
+    else if (depth < 50) {
+        return "yellow"
+    }
+    else if (depth < 70) {
+        return "orange"
+    }
+    else if (depth < 90) {
+        return "red"
+    }
+    else {return "darkred"}
+}
 // Creating our initial map object:
 // We set the longitude, latitude, and starting zoom level.
 // This gets inserted into the div with an id of "map".
@@ -11,48 +37,19 @@ var myMap = L.map("map", {
     center: [38.58, -121.49],
     zoom: 5
   });
-  
-  // Adding a tile layer (the background map image) to our map:
-  // We use the addTo() method to add objects to our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+
+// Adding a tile layer (the background map image) to our map:
+// We use the addTo() method to add objects to our map.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
   
 // Add earthquake data
 d3.json(url).then(function(data) {
     console.log(data)
-    // Create the loop to add markers for each earthquake
+    // Create a loop
     for (var i = 0; i < data.features.length; i++){
-        // Find the coordinates for the marker
-        coord = [data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]]
-
-        // Add the color for each marker
-        var color = "";
-        var depth = data.features[i].geometry.coordinates[2];
-        function markerSize(magnitude) {
-            console.log(magnitude)
-            return magnitude * 10000;
-        }
-    
-        function markerColor(depth) {
-            if (depth < 10) {
-                return "green"
-            }
-            else if (depth < 30) {
-                return "yellow-green"
-            }
-            else if (depth < 50) {
-                return "yellow"
-            }
-            else if (depth < 70) {
-                return "orange"
-            }
-            else if (depth < 90) {
-                return "red-orange"
-            }
-            else {
-                return "red"
-            }
-        }
+        // Find the coordinate for each earthquake
+        coords = [data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]]
     }
 });
